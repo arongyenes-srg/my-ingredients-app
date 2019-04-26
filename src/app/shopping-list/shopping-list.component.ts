d7121bc246527ec2f071a25f586e88f386df1bce
arongyenes-srg <aron.gyenes@srg.hu>
2019-04-26 09:58:55 +0200

    add content to components

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../shared/ingredient.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.sass']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('salt', 1000),
+    new Ingredient('water', 4)
+  ];
 
   constructor() { }
 
