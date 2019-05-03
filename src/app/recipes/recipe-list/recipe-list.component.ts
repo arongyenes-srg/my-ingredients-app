9befe0b25d359610549aad64c91c04044d5d8769
arongyenes-srg <aron.gyenes@srg.hu>
2019-05-03 17:03:09 +0200

    add recipe services

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,21 +9,11 @@ import { Recipe } from '../recipe.model';
 })
 
 export class RecipeListComponent implements OnInit {
-  @Output() itemWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[];
 
-  recipes: Recipe[] = [
-    new Recipe('first recipe', 'nice', 'https://images.media-allrecipes.com/userphotos/600x600/1124412.jpg'),
-    new Recipe('second recipe', 'well', 'https://res.cloudinary.com/hellofresh/image/upload/f_auto,fl_lossy,h_436,q_auto/v1/hellofresh_s3/image/5ad0b4c3ae08b51e0e19ff34-14952d03.jpg'),
-    new Recipe('third recipe', 'rare', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdOL580uoMPLXi9U8dUEeBfyHi4KmnbnRIQKuAS87cmgjKWqjQ1A'),
-    new Recipe('fourth recipe', 'lethal', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMp5b4aDO-LOYYsB1taizB_Crs1A-VTv7Z0rq6vgXwKHisiAan')
-  ];
-
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
-  }
-
-  onSelected(item: Recipe) {
-    this.itemWasSelected.emit(item);
+    this.recipes = this.recipeService.getRecipes();
   }
 }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,17 @@
+import { Recipe } from './recipe.model';
+import { EventEmitter } from '@angular/core';
+
+export class RecipeService {    
+    recipeSelected = new EventEmitter<Recipe>();
+
+    private recipes: Recipe[] = [
+        new Recipe('first recipe', 'nice', 'https://images.media-allrecipes.com/userphotos/600x600/1124412.jpg'),
+        new Recipe('second recipe', 'well', 'https://res.cloudinary.com/hellofresh/image/upload/f_auto,fl_lossy,h_436,q_auto/v1/hellofresh_s3/image/5ad0b4c3ae08b51e0e19ff34-14952d03.jpg'),
+        new Recipe('third recipe', 'rare', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdOL580uoMPLXi9U8dUEeBfyHi4KmnbnRIQKuAS87cmgjKWqjQ1A'),
+        new Recipe('fourth recipe', 'lethal', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMp5b4aDO-LOYYsB1taizB_Crs1A-VTv7Z0rq6vgXwKHisiAan')
+    ];
+
+    getRecipes() {
+        return this.recipes.slice();
+    }
+}
